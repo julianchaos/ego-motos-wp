b16a4eab31b5e3eb7708df8645b5a7ca54fde7d6
Julian Andrade <julianchaos@gmail.com>
2015-11-22 20:40:05 -0200

    Slider da home apresentando as imagens mobile e desktop.

--- page-templates/home-carousel.php
+++ page-templates/home-carousel.php
@@ -1,25 +1,52 @@
+<?php
+$sliderItens = array();
+
+$query = new WP_Query(array(
+	'post_type' => 'home-slider',
+	'posts_per_page' => -1
+));
+while($query->have_posts())
+{
+	$query->the_post();
+	
+	$sliderItens[] = array(
+		'titulo' => get_the_title(),
+		'legenda' => get_field('legenda'),
+		'link' => get_field('link'),
+		'img-mobile' => get_field('imagem_mobile'),
+		'img-desktop' => get_field('imagem_desktop')
+	);
+}
+
+?>
 <section class="home-carousel">
 	<div id="carousel-main" class="carousel slide" data-ride="carousel">
+		
 		<!-- Indicators -->
 		<ol class="carousel-indicators">
-			<li data-target="#carousel-main" data-slide-to="0" class="active"></li>
-			<li data-target="#carousel-main" data-slide-to="1"></li>
+<?php
+for ($i = 0; $i < count($sliderItens); $i++) {
+	$active = $i === 0 ? "class='active'" : null;
+	echo "<li data-target='#carousel-main' data-slide-to='$i' $active ></li>";
+}
+?>
 		</ol>
 
 		<!-- Wrapper for slides -->
 		<div class="carousel-inner" role="listbox">
-			<div class="item active">
-				<img src="..." alt="...">
-				<div class="carousel-caption">
-					...
-				</div>
-			</div>
-			<div class="item">
-				<img src="..." alt="...">
-				<div class="carousel-caption">
-					...
-				</div>
-			</div>
+<?php
+foreach($sliderItens as $i => $item)
+{
+	$active = $i === 0 ? "active" : null;
+	echo "<div class='item $active'>";
+		echo "<img src='{$item['img-mobile']}' alt='{$item['titulo']}' class='img-mobile' />";
+		echo "<img src='{$item['img-desktop']}' alt='{$item['titulo']}' class='img-desktop' />";
+		
+		echo "<div class='carousel-caption'>";
+		echo "</div>";
+	echo "</div>";
+}
+?>
 		</div>
 
 		<!-- Controls -->
