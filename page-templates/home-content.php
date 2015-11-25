617ddf405cc2984e61e771f34dfc4bc9d9ed37f6
Julian Andrade <julianchaos@gmail.com>
2015-11-24 23:34:17 -0200

    Conteúdo da home (listagem de linhas de motos) atendendo ao layout de telas mobile (extra small)

---
+++ page-templates/home-content.php
@@ -0,0 +1,42 @@
+<section id="home-content">
+	<div class='container'>
+		<div class="row">
+			<div class="col-xs-12">
+				<a href="javascript: void(0)" class="moto super-sport">
+					<img src="<?php echo get_template_directory_uri() ?>/images/home/01.super-sport.png" alt="Super Sport" />
+					<h2>Super Sport</h2>
+				</a>
+			</div>
+			<div class="col-xs-12">
+				<a href="javascript: void(0)" class="moto street">
+					<img src="<?php echo get_template_directory_uri() ?>/images/home/02.street.png" alt="Street" />
+					<h2>Street</h2>
+				</a>
+			</div>
+			<div class="col-xs-12">
+				<a href="javascript: void(0)" class="moto couston">
+					<img src="<?php echo get_template_directory_uri() ?>/images/home/03.couston.png" alt="Street" />
+					<h2>Couston</h2>
+				</a>
+			</div>
+			<div class="col-xs-12">
+				<a href="javascript: void(0)" class="moto dual-purpose">
+					<img src="<?php echo get_template_directory_uri() ?>/images/home/04.dual-purpose.png" alt="Street" />
+					<h2>Dual Purpose</h2>
+				</a>
+			</div>
+			<div class="col-xs-12">
+				<a href="javascript: void(0)" class="moto scooter">
+					<img src="<?php echo get_template_directory_uri() ?>/images/home/05.scooter.png" alt="Street" />
+					<h2>Scooter</h2>
+				</a>
+			</div>
+			<div class="col-xs-12">
+				<a href="javascript: void(0)" class="moto conheca-linha">
+					<!--<img src="<?php echo get_template_directory_uri() ?>/images/home/transparent.png" alt="" />-->
+					<h2>Conheça toda a linha</h2>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -4,5 +4,6 @@
 get_header();
 
 get_template_part('page-templates/home', 'carousel');
+get_template_part('page-templates/home', 'content');
 
 get_footer();
