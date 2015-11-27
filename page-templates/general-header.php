d8fd312b5fdf3ed283ad54d880fbeefea8396dcf
Julian Andrade <julianchaos@gmail.com>
2015-11-26 23:22:26 -0200

    Template para página "A Ego Motos". Template padronizado para cabeçalho com texto e imagem.

---
+++ page-templates/general-header.php
@@ -0,0 +1,7 @@
+<section id="general-header" style="background-image: url('<?php the_field('destaque_cabecalho_image') ?>');">
+	<div>
+		<div>
+			<?php the_field('destaque_cabecalho_texto') ?>
+		</div>
+	</div>
+</section>
---
+++ page-templates/page-ego-motos.php
@@ -0,0 +1,6 @@
+<?php
+// Template Name: A Ego Motos
+
+get_header();
+get_template_part('page-templates/general', 'header');
+get_footer();
