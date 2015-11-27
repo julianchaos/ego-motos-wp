1c1e410b4f76a5f8f8757c90ec5ce9b812797508
Julian Andrade <julianchaos@gmail.com>
2015-11-27 00:28:28 -0200

    Fonte do conteúdo da página "Ego Motos", se ajustando para todas as resoluções.

---
+++ page-templates/ego-motos-content.php
@@ -0,0 +1,9 @@
+<section id="ego-motos-content">
+	<div class='container'>
+		<div class='row'>
+			<div class='col-xs-12 col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-3'>
+				<?php the_content() ?>
+			</div>
+		</div>
+	</div>
+</section>
--- page-templates/page-ego-motos.php
+++ page-templates/page-ego-motos.php
@@ -2,5 +2,12 @@
 // Template Name: A Ego Motos
 
 get_header();
-get_template_part('page-templates/general', 'header');
+
+while ( have_posts() ) {
+	the_post();
+				
+	get_template_part('page-templates/general', 'header');
+	get_template_part('page-templates/ego-motos', 'content');
+}
+
 get_footer();
