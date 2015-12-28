5364ba7725af9e367d69e09af8f6666cc086c3f5
Julian Andrade <julianchaos@gmail.com>
2015-12-28 15:13:42 -0200

    Formulário de contato sendo adicionado como um campo personalizado.

--- page-templates/contato-formulario-contato.php
+++ page-templates/contato-formulario-contato.php
@@ -4,7 +4,7 @@
 			<h2>Formulário de <span>Contato</span></h2>
 			<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-6 col-md-offset-3">
 				<div class="row">
-					<?php echo do_shortcode('[contact-form-7 id="104" title="Formulário de contato"]') ?>
+					<?php the_field('formulario_contato') ?>
 				</div>
 			</div>
 		</div>
