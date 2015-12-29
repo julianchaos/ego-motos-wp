2c90ab8f8a8dbd5efd59f500e8a46ab91500accc
Julian Andrade <julianchaos@gmail.com>
2015-12-29 16:32:31 -0200

    Cabeçalho (imagem do modelo e texto de chamada) da página single de modelos, com layout atendendo à todos os breakpoints

---
+++ page-templates/modelo-header.php
@@ -0,0 +1,12 @@
+<section id="modelo-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+				<?= is_string(get_field('imagem_modelo')) ? "<img src='".get_field('imagem_modelo')."' />" : null ?>
+				<p><?php the_field('chamada_curta') ?></p>
+			</div>
+		</div>
+	</div>
+</section>
+
+
---
+++ single-modelo.php
@@ -0,0 +1,7 @@
+<?php
+
+get_header();
+
+get_template_part('page-templates/modelo', 'header');
+
+get_footer();
