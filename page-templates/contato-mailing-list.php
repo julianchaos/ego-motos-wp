e5522517966a20852a9f6d07e07bf269ccb585b9
Julian Andrade <julianchaos@gmail.com>
2015-12-29 10:32:07 -0200

    Seção de adição de email no mailing list (página contato).

---
+++ page-templates/contato-mailing-list.php
@@ -0,0 +1,10 @@
+<section id="contato-mailing-list">
+	<div class="container">
+		<div class="row">
+			<h3>Receba nossas novidades</h3>
+			<div class="col-xs-12">
+				<?php the_field('formulario_mailing_list') ?>
+			</div>
+		</div>
+	</div>
+</section>
--- page-templates/page-contato.php
+++ page-templates/page-contato.php
@@ -7,5 +7,6 @@ get_template_part('page-templates/general', 'header');
 get_template_part('page-templates/contato', 'mapa');
 get_template_part('page-templates/contato', 'nossos-contatos');
 get_template_part('page-templates/contato', 'formulario-contato');
+get_template_part('page-templates/contato', 'mailing-list');
 
 get_footer();
