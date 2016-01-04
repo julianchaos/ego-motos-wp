1997377132a48b66ebde6f3d76b4e144be82b3dc
Julian Andrade <julianchaos@gmail.com>
2016-01-04 16:09:44 -0200

    Modificações na página single de modelos 0km:  - Vídeo embed dinâmico  - URL do vídeo dinâmico  - URL de sharer do Facebook

--- page-templates/modelo-video.php
+++ page-templates/modelo-video.php
@@ -1,13 +1,17 @@
+<?php
+$pageEncodedUrl = urlencode(get_permalink());
+$sharerUrl = "https://www.facebook.com/dialog/share?app_id=1538517343132107&display=popup&href=$pageEncodedUrl&redirect_uri=$pageEncodedUrl";
+?>
 <section id="modelo-video">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
 				<div class="video-container">
-					<iframe width="420" height="315" src="https://www.youtube.com/embed/6T8jSBBaah4?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+					<?php the_field('video_embed') ?>
 					<div class="btn-container">
 						<a href="https://www.facebook.com/EgoMotos" target="_blank" class="btn-curtir"></a>
-						<a href="javascript: void(0)" target="_blank" class="btn-ver-depois"></a>
-						<a href="javascript: void(0)" target="_blank" class="btn-compartilhar"></a>
+						<a href="<?php the_field('video_url') ?>" target="_blank" class="btn-ver-depois"></a>
+						<a href="<?php echo $sharerUrl ?>" class="btn-compartilhar"></a>
 					</div>
 				</div>
 			</div>
