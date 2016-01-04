ecf3febdd88fb737bb346fd322b188f690a93a0f
Julian Andrade <julianchaos@gmail.com>
2016-01-04 14:55:39 -0200

    Layout do vídeo e botões de interação para telas até 768px (extra small)

---
+++ page-templates/modelo-video.php
@@ -0,0 +1,16 @@
+<section id="modelo-video">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="video-container">
+					<iframe width="420" height="315" src="https://www.youtube.com/embed/6T8jSBBaah4?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+					<div class="btn-container">
+						<a href="https://www.facebook.com/EgoMotos" target="_blank" class="btn-curtir"></a>
+						<a href="javascript: void(0)" target="_blank" class="btn-ver-depois"></a>
+						<a href="javascript: void(0)" target="_blank" class="btn-compartilhar"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
--- single-modelo.php
+++ single-modelo.php
@@ -5,6 +5,7 @@ get_header();
 get_template_part('page-templates/modelo', 'header');
 get_template_part('page-templates/modelo', 'animacao');
 get_template_part('page-templates/modelo', 'fotos');
-
+get_template_part('page-templates/modelo', 'form-cotacao');
+get_template_part('page-templates/modelo', 'video');
 
 get_footer();
