ecc8b9bf0a58c17ca508e7c212ffa7e753790f1b
Julian Andrade <julianchaos@gmail.com>
2016-01-04 23:04:44 -0200

    Botão para formulário de orçamento (página single de modelos) atendendo à todos os breakpoints

---
+++ page-templates/modelo-form-cotacao.php
@@ -0,0 +1,11 @@
+<section id="modelo-form-cotacao">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div>
+					<a href="javascript: void(0)" class="btn-cotacao">Faça sua cotação</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
