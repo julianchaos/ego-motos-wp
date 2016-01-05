6f20570aa1c7949425b535ab80c3d6227d88535e
Julian Andrade <julianchaos@gmail.com>
2016-01-05 14:09:12 -0200

    Estilo para formulário de orçamento (single modelos)

--- page-templates/modelo-form-cotacao.php
+++ page-templates/modelo-form-cotacao.php
@@ -5,7 +5,9 @@
 				<div>
 					<a href="#form-cotacao-popup" class="btn-cotacao open-popup-link">Faça sua cotação</a>
 					<div id="form-cotacao-popup" class="mfp-hide white-popup-block">
-						teste
+						<h3>Ficou interessado?</h3>
+						<p>Preencha o formulário que a Ego Motos retornará o mais breve possível</p>
+						<?php echo do_shortcode('[contact-form-7 id="126" title="Formulário Cotação"]') ?>
 					</div>
 				</div>
 				
