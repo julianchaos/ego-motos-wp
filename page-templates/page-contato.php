6d3a24e2970b3fa8f46ad9a061f06df79b6f328e
Julian Andrade <julianchaos@gmail.com>
2016-02-19 13:48:42 -0200

    Reordenação das seções da página de contato

--- page-templates/page-contato.php
+++ page-templates/page-contato.php
@@ -4,9 +4,9 @@
 get_header();
 
 get_template_part('page-templates/general', 'header');
-get_template_part('page-templates/contato', 'mapa');
-get_template_part('page-templates/contato', 'nossos-contatos');
 get_template_part('page-templates/contato', 'formulario-contato');
-get_template_part('page-templates/contato', 'mailing-list');
+get_template_part('page-templates/contato', 'nossos-contatos');
+get_template_part('page-templates/contato', 'mapa');
+//get_template_part('page-templates/contato', 'mailing-list');
 
 get_footer();
